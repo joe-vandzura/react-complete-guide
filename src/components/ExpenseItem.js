24995313a69d7d8a9886994d09e02717555c78cb
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-03-29 22:51:42 -0500

    Completed up to part 36 of complete react guide after props lesson and exercise

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   const expenseDate = new Date(2023, 3, 29);
   const expenseTitle = 'Rabbit';
   const expensePrice = 2000.00;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
