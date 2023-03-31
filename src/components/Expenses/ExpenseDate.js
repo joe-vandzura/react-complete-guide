ce5b7b603d2aca934b2eaf191f285e2ea4a1d83d
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-03-30 23:28:01 -0500

    finished up to part 54 of complete react guide

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card"; 
